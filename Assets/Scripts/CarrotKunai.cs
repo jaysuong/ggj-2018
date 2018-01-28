194f7867502e443c5811d4794dd43c3f6249882c
Porrith Suong <psuong95@gmail.com>
2018-01-28 13:37:33 -0500

    Adds AimX, AImY to the input manager.

--- Assets/Scripts/CarrotKunai.cs
+++ Assets/Scripts/CarrotKunai.cs
@@ -8,16 +8,12 @@ public class CarrotKunai : MonoBehaviour {
 	private Rigidbody rb;
 	public float InitialForce = 1000;
 	public float damage = 2;
+
 	// Use this for initialization
 	void Start () {
 		rb = gameObject.GetComponent<Rigidbody> ();
 		rb.transform.Rotate (270f, 0f, 0f);
 		rb.AddForce (transform.up * -InitialForce);
-	}
-
-	// Update is called once per frame
-	void Update () {
-
 	}
 
 	void OnCollisionEnter (Collision c) {
