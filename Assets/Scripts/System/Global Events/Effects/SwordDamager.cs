e58b037b8751851e262473fd374615cd21383b14
Jay Suong <psuong95@gmail.com>
2018-01-28 14:07:08 -0500

    Added a sword event

---
+++ Assets/Scripts/System/Global Events/Effects/SwordDamager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace boc {
+	public class SwordDamager : MonoBehaviour {
+		[SerializeField]
+		private float explosionRadius;
+		[SerializeField]
+		private LayerMask explosionMask;
+		[SerializeField]
+		private float damage;
+
+		private new Rigidbody rigidbody;
+
+		private void OnTriggerEnter (Collider other) {
+			rigidbody.isKinematic = true;
+			rigidbody.velocity = Vector3.zero;
+
+			var colliders = Physics.OverlapSphere (transform.position, explosionRadius, explosionMask);
+
+			for (var i = 0; i < colliders.Length; ++i) {
+				var hp = colliders[i].GetComponent<IHealth> ();
+				if (hp != null) {
+					hp.Damage (damage);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/System/Global Events/SwordsGlobalEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace boc {
+	[CreateAssetMenu(menuName = "ScriptableObjects/Global/Swords")]
+	public class SwordsGlobalEvent : GlobalEvent {
+		[SerializeField]
+		private GameObject swordPrefab;
+		[SerializeField]
+		private Vector3 startPosition;
+		[SerializeField]
+		private float spawnDistance;
+		[SerializeField]
+		private int numberToSpawn;
+		[SerializeField]
+		private float interval;
+
+		private int amountSpawned;
+		private float timer;
+
+		public override void OnEventStart (GlobalEventManager manager) {
+			timer = 0f;
+		}
+
+		public override void OnEventUpdate (GlobalEventManager manager) {
+			timer -= Time.deltaTime;
+
+			if (timer <= 0f) {
+				timer = interval;
+
+				var random = Random.insideUnitSphere * spawnDistance;
+				random.y = 0f;
+				var position = startPosition + random;
+
+				Instantiate (swordPrefab, position, Quaternion.identity);
+			}
+		}
+
+	}
+}
