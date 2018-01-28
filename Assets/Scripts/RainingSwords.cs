99cf718deffd9b92901cc482542b9d3e99321fef
dqdt <sw2384@nyu.edu>
2018-01-28 13:11:55 -0500

    bunny with eyes (script folder)

---
+++ Assets/Scripts/RainingSwords.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainingSwords : MonoBehaviour {
+	[SerializeField] GameObject sword;
+	//GameObject[] SwordArray = new 
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
