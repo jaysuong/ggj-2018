7bc73b7e06d432c0eb02b5a4fe296bf0fb28cff9
Jay Suong <psuong95@gmail.com>
2018-01-28 12:16:28 -0500

    Added idling

---
+++ Assets/Scripts/AI/Actions/Idle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using InitialPrefabs.DANI;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace boc {
+	public class Idle : Action {
+		[SerializeField]
+		private float idleTime;
+
+		private float timer;
+		private NavMeshAgent agent;
+
+		public override void OnStart () {
+			agent = GetComponent<NavMeshAgent> ();
+		}
+
+		public override void OnActionStart () {
+			timer = idleTime;
+			agent.isStopped = true;
+		}
+
+		public override ActionState OnActionUpdate () {
+			timer -= Time.deltaTime;
+
+			return timer <= 0f ? ActionState.Success : ActionState.Running;
+		}
+
+	}
+}
