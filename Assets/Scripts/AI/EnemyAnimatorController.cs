ecd9aac2441bdd2892a63db961b0b6c64f7d2ef3
Porrith Suong <psuong95@gmail.com>
2018-01-28 14:29:13 -0500

    Adds move triggers.

---
+++ Assets/Scripts/AI/EnemyAnimatorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAnimatorController : MonoBehaviour {
+
+	public string speed = "Speed";
+
+	private Animator animator;
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update () {
+		animator.SetFloat (speed, agent.velocity.magnitude);
+	}
+}
